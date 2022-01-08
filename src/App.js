9c94272e9af5c34528084d5768ba02852da2f06a
Maciej Lewicki <lewickimapl@gmail.com>
2022-01-08 13:22:44 -0500

    add multiple orb capabilities

--- src/App.js
+++ src/App.js
@@ -10,12 +10,31 @@ const useQuery = () => {
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 
+const labels = [
+  'Bligatory Score',
+  'Community Score',
+  'Team Score'
+];
+
 // only here for router purposes atm
 const OrbWrapper = () => {
   let query = useQuery();
 
+  const percents = query.get('percent').split(',');
+  const colors = query.get('color').split(',');
+  const sizes = query.get('size').split(',');
+
   return (
-    <Orb colour={query.get('color')} percent={parseFloat(query.get('percent'))} size={parseInt(query.get('size'))} />
+    <div style={{display: 'flex', flexFlow: 'row nowrap', justifyContent: 'space-between', alignItems: 'center'}}>
+      {percents.map((perc, index) => (
+        <Orb
+          colour={colors[index] ?? colors[0] ?? 'red'}
+          percent={perc}
+          size={sizes[index] ?? sizes[0] ?? 200}
+          label={labels[index] ?? labels[0]}
+        />
+      ))}
+    </div>
   );
 }
 
--- src/Orb.js
+++ src/Orb.js
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 
-const Orb = ({ colour, percent, size }) => {
+const Orb = ({ colour, percent, size, label }) => {
   const [pct, setPct] = useState(0);
 
   const r = size / 2 - (size / 10);
@@ -21,31 +21,32 @@ const Orb = ({ colour, percent, size }) => {
     : colour;
 
   return (
-    <>
-    <svg style={{position: 'absolute', width: `${size}px`, height: `${size}px`, top: 0, left: 0}}>
-      <style type="text/css">
-        .st0{`{fill:#FFFFFF;stroke:#000000;stroke-width:${Math.max(size / 100, 1.5)};stroke-linejoin:round;stroke-miterlimit:10;}`}
-      </style>
-      <circle cx={size / 2} cy={size / 2} r={r + size / 20} />
-      <image href='resources/bligatory-orb-base.png' width={size} height={size} />
-      <text textAnchor='middle' transform={`matrix(1 0 0 1 ${size / 2} ${size / 2 + size / 20})`} fontSize={size / 8.5} className='st0 svg-text'>{percent}%</text>
-      <image href='resources/bligatory-orb-tape.png' width={size} height={size} />
-      <g transform={`rotate(-90 ${size / 2} ${size / 2})`}>
-        <circle
-          r={r}
-          cx={size / 2}
-          cy={size / 2}
-          fill='transparent'
-          stroke={strokePct !== circ ? color : ''}
-          strokeWidth={size / 20}
-          strokeDasharray={circ}
-          strokeDashoffset={percent ? strokePct : 0}
-          strokeLinecap='butt'
-          style={{transition: 'all 0.7s ease-in-out'}}
-        />
-      </g>
-    </svg>
-    </>
+    <div style={{display: 'flex', flexFlow: 'column nowrap', justifyContent: 'space-between', alignItems: 'center'}}>
+      <svg style={{position: 'relative', width: `${size}px`, height: `${size}px`}}>
+        <style type="text/css">
+          .st0{`{fill:#FFFFFF;stroke:#000000;stroke-width:${Math.max(size / 100, 1.5)};stroke-linejoin:round;stroke-miterlimit:10;}`}
+        </style>
+        <circle cx={size / 2} cy={size / 2} r={r + size / 20} />
+        <image href='resources/bligatory-orb-base.png' width={size} height={size} />
+        <text textAnchor='middle' transform={`matrix(1 0 0 1 ${size / 2} ${size / 2 + size / 20})`} fontSize={size / 8.5} className='st0 svg-text'>{percent}%</text>
+        <image href='resources/bligatory-orb-tape.png' width={size} height={size} />
+        <g transform={`rotate(-90 ${size / 2} ${size / 2})`}>
+          <circle
+            r={r}
+            cx={size / 2}
+            cy={size / 2}
+            fill='transparent'
+            stroke={strokePct !== circ ? color : ''}
+            strokeWidth={size / 20}
+            strokeDasharray={circ}
+            strokeDashoffset={percent ? strokePct : 0}
+            strokeLinecap='butt'
+            style={{transition: 'all 0.7s ease-in-out'}}
+          />
+        </g>
+      </svg>
+      <h1>{label}</h1>
+    </div>
   );
 }
 
